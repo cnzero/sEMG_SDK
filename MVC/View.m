866e056556150552d1ac66d607b7bb7fe410591b
zero <zerolee18@gmail.com>
2017-01-11 22:26:38 +0800

    mkdir folders

--- MVC/View.m
+++ MVC/View.m
@@ -1,26 +1,17 @@
 classdef View < handle
-	
 	properties
-		% -- basic parameters on device
 		hChannels = [1 2];
 		hPartSelection = {'Upper'}
 
 		hFigure
-		% --EMG
 		hAxesEMG = []
 		hPlotsEMG = []
 		hPanelEMG
+		folder_name = []
 
 		% --guiding pictures to display
 		% hPictureReady
 		hAxesPictureBed
-		% nthPicture = 1
-		% hPicturesStack = {'Snooze', ...
-						  % 'Grasp', ...
-						  % 'Snooze', ...
-						  % 'Open', ...
-						  % 'End'};
-		% hTimerPictures
 
 		% --Buttons
 		hButtonStart
@@ -35,14 +26,14 @@ classdef View < handle
 		% --Construct
 		function obj = View(modelObj)
 			% ===================basic & static figure displays=================
-			addpath('..\MaxFigure');addpath('..\pictures');
+			addpath('..\MaxFigure');addpath('..\pictures');addpath('..\Users');
+			obj.Init_Folder();
 			obj.hFigure = figure();maximize(obj.hFigure);
 			obj.hPanelEMG = uipanel('Parent', obj.hFigure, ...
 								  'Units', 'normalized', ...
 								  'Position', [0 0 0.5 0.96]);
 			% --hAxesEMG, hPlotsEMG
-			% nChannels = length(obj.hChannels);
-			nChannels = 2;
+			nChannels = length(obj.hChannels);
 			dWidth= 0.95/nChannels;
 			for ch=1:nChannels
 				obj.hAxesEMG(ch) = axes('Parent', obj.hPanelEMG, ...
@@ -58,18 +49,7 @@ classdef View < handle
 									   'Position', [0.51 0.45 0.48 0.5]);
 			hPictureReady = imread('Ready.jpg');
 			imshow(hPictureReady, 'Parent', obj.hAxesPictureBed);
-			% obj.hTimerPictures = timer('Period', 2, ...
-									  % 'ExecutionMode', 'fixedSpacing', ...
-									  % 'TasksToExecute', length(obj.hPicturesStack));
-									  % 'TimerFcn', {@TimerFcn_PicturesChanging, obj});
-			% start(obj.hTimerPictures);
-			%  --- part selection
-			% namePartSelection = {'Body', 'Upper'};
-			% hPopupPartSelection = uicontrol('Parent', obj.hFigure, ...
-			% 								'Style', 'popupmenu', ...
-			% 								'Units', 'normalized', ...
-			% 								'Position', [0.52 0.4 0.04 0.02], ...
-			% 								'String', namePartSelection);
+
 			obj.hButtonStart = uicontrol('Parent', obj.hFigure, ...
 									 	 'Style', 'pushbutton', ...
 									 	 'Units', 'normalized', ...
@@ -103,28 +83,52 @@ classdef View < handle
         	set(obj.hButtonAnalyze, 'Callback', @controller.Callback_ButtonAnalyze);
         end
 
+        function Init_Folder(obj)
+        	c = clock;
+        	folder_name	= [];
+			folder_name = [folder_name, ...
+						   num2str(c(1)), ... % year
+			               '_', ...
+			               num2str(c(2)), ... % month
+			               '_', ...
+			               num2str(c(3)), ... % day
+			               '_', ...
+			               num2str(c(4)), ... % hour
+			               '_', ...
+			               num2str(c(5)), ... % minute
+			               '_', ...
+			               num2str(fix(c(6)))]; % second
+			% for example, run this code at 2016-07-13 10:13:30s
+			% folder_name, 2016_07_13_10_13_30
+			mkdir(['..\Users\',folder_name]);
+			mkdir(['..\Users\',folder_name, '\EMG']);
+			mkdir(['..\Users\',folder_name, '\ACC']);
+			obj.folder_name = ['..\Users\',folder_name];
+        end
         % -- event [dataEMGChanged] responde function
         function UpdateAxesEMG(obj, source, event)
-        	% disp('UpdateAxesEMG...');
-        	if(length(obj.dataAxesEMG) < 32832)
-        		obj.dataAxesEMG = [obj.dataAxesEMG; ...
-        						   obj.modelObj.dataEMG];
-        	else
-        		obj.dataAxesEMG = [obj.dataAxesEMG(length(obj.modelObj.dataEMG)+1:end); ...
-        						   obj.modelObj.dataEMG];
-        	end
-        	% --==seperate [obj.dataAxesEMG] into 16-Channel-Axes
-        	for ch=1:length(obj.hPlotsEMG)
-        		data_ch = obj.dataAxesEMG(ch:16:end);
-        		% set(obj.hPlotsEMG(ch), 'Ydata', data_ch);
-        		plot(obj.hAxesEMG(ch), data_ch);
-        		drawnow;
-        	end
+        	disp('UpdateAxesEMG...');
+        	% if(length(obj.dataAxesEMG) < 32832)
+        	% 	obj.dataAxesEMG = [obj.dataAxesEMG; ...
+        	% 					   obj.modelObj.dataEMG];
+        	% else
+        	% 	obj.dataAxesEMG = [obj.dataAxesEMG(length(obj.modelObj.dataEMG)+1:end); ...
+        	% 					   obj.modelObj.dataEMG];
+        	% end
+        	% % --==seperate [obj.dataAxesEMG] into 16-Channel-Axes
+        	% for ch=1:length(obj.hPlotsEMG)
+        	% 	data_ch = obj.dataAxesEMG(ch:16:end);
+        	% 	% set(obj.hPlotsEMG(ch), 'Ydata', data_ch);
+        	% 	plot(obj.hAxesEMG(ch), data_ch);
+        	% 	drawnow;
+        	% end
+        	% ---=== It costs much time to refresh axes in this way.
         end
         % -- event [dataEMGChanged] responde function
         function Write2FilesEMG(obj, source, event)
         	% disp('Write2FilesEMG...');
         	% --==write [obj.modelObj.dataEMG] into txt file with appending format.
+        	for index=1:length(obj.)
         end
 
 	end
