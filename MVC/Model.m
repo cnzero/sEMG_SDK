09d5f26de86c09ec67340093412ea9a4051274ce
zero <zerolee18@gmail.com>
2017-01-12 16:23:34 +0800

    increase 'BytesAvailableFcnCout' to make more time for others

--- MVC/Model.m
+++ MVC/Model.m
@@ -15,11 +15,13 @@ classdef Model <  handle
 			obj.interfaceObjects ={...
 								   tcpip(HOST_IP, 50040), ...   % common
 					   			   tcpip(HOST_IP, 50041, ...    % EMG
-								   'InputBufferSize', 6400, ...
+								   'InputBufferSize', 64000, ...
 								   'BytesAvailableFcnMode', 'byte', ...
-								   'BytesAvailableFcnCount', 1728, ...
+								   'BytesAvailableFcnCount', 1728*10, ...
 								   'BytesAvailableFcn', {@obj.NotifyEMG}) ...
 								   };
+			fopen(obj.interfaceObjects{1});
+			fopen(obj.interfaceObjects{2});
 		end
 		
 		% -- events trigger
@@ -41,8 +43,6 @@ classdef Model <  handle
 		% ---=== Start() & Stop()
 		function Start(obj)
 			try
-				fopen(obj.interfaceObjects{1});
-				fopen(obj.interfaceObjects{2});
 				fprintf(obj.interfaceObjects{1}, sprintf(['START\r\n\r']));
 			catch
 				error('Connection error.');
